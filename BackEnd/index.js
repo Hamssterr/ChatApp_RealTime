c6e21b03b5812921deffa8203d49294d7ebb2bd9
Hamssterr <dancamu1002@gmail.com>
2025-09-15 21:46:53 +0700

    Update

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -72,11 +72,15 @@ app.use("/api/messages", messageRouter);
 // Connect to MongoDB
 await connectDB();
 
+
+
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 8080;
   server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
+
+
 
 export default server;
