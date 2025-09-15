c8b3ab983c6aaf24fff86332b8f963f42a914154
Hamssterr <dancamu1002@gmail.com>
2025-09-15 20:13:06 +0700

    Update env

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -4,33 +4,21 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import { Server } from "socket.io";
-import dotenv from "dotenv";
 
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
-
-// Load environment variables
-dotenv.config();
 
 // Create Express app and HTTP server
 const app = express();
 const server = http.createServer(app);
 
-// Parse allowed origins from environment variable
-const allowedOrigins = process.env.ALLOWED_ORIGINS;
-
 // Initialize Socket.io
 export const io = new Server(server, {
   cors: {
-    origin: allowedOrigins,
+    origin: process.env.ALLOWED_ORIGINS,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Access-Control-Allow-Origin",
-      "Access-Control-Allow-Credentials",
-    ],
+    allowedHeaders: ["Content-Type", "Authorization"],
   },
 });
 
@@ -46,10 +34,10 @@ io.on("connection", (socket) => {
     userSocketMap[userId] = socket.id;
   }
 
-  // Emit online users
+  //  Emit online users
   io.emit("online-users", Object.keys(userSocketMap));
 
-  // Emit offline users
+  //  Emit offline users
   socket.on("disconnect", () => {
     console.log("User disconnected with ID:", userId);
     delete userSocketMap[userId];
@@ -61,7 +49,7 @@ io.on("connection", (socket) => {
 app.use(express.json({ limit: "4mb" }));
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: process.env.ALLOWED_ORIGINS,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
