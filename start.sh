4530edac4d029c0e30a432cd298f2de442fe3b45
Hamssterr <dancamu1002@gmail.com>
2025-09-20 15:50:20 +0700

    .....

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
-cd ChatApp_RealTime/BackEnd
-npm install
-npm run build
-npm start
-
+FROM node:18
+WORKDIR /app/BackEnd
+COPY BackEnd/package*.json ./
+RUN npm install
+COPY BackEnd/ .
+EXPOSE 3000
+CMD ["npm", "start"]
