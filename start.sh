ca78be0e5edbd5c4159c95792c72ec6bdb7086c6
Hamssterr <dancamu1002@gmail.com>
2025-09-20 15:38:45 +0700

    Test

--- start.sh
+++ start.sh
@@ -1,4 +1,26 @@
 cd BackEnd
-npm install
-npm run build
-npm start
+# npm install
+# npm run build
+# npm start
+
+# Use the official Node.js 20 image as the base
+FROM node:20
+
+# Set the working directory inside the container
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json (if available)
+COPY package*.json ./
+
+# Install dependencies
+RUN npm ci
+
+# Install FFmpeg
+RUN apt-get update && apt-get install -y ffmpeg && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Copy the rest of the application files
+COPY . ./
+
+
+# Command to run the application
+CMD ["npm", "run", "start"]
